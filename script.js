05c710838ddde555c26673b94048f55a54a2532e
Katy Smith <113362369+catrionafsmith@users.noreply.github.com>
2023-01-31 13:57:39 +1100

    JS for learn page moved and hover effect added

--- script.js
+++ script.js
@@ -8,6 +8,8 @@
 //     });
 // });
 
+
+//  Functionality of form on Volunteer page
 document.addEventListener("DOMContentLoaded", function(){
     var submitBtn = document.getElementById("submit-btn");
     var contentDiv = document.getElementById("content");
@@ -24,3 +26,39 @@ document.addEventListener("DOMContentLoaded", function(){
         }
     });
 });
+
+// Functionality of cards on Learn Page
+// This line targets the class learnContainer, which is on the Learn page, so that the cards are added into this container when created.
+const learnCards = document.querySelector(".learnContainer")
+//This is the createCard function which determines the structure of the cards on the Learn page, and adds classes to each section.
+function createCard([img, title, description, link]) {
+    let code = `
+    
+    <div class="col">        
+            <div class="card learnCard">
+                <img src="${img}" class="card-img-top1" alt="${title}">
+                <div class="card-body">
+                    <h5 class="card-title">${title}</h5>
+                    <p class="card-text">${description}</p>
+                    <a href="${link}" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+    </div>
+    
+
+    `;
+    learnCards.innerHTML += code;
+}
+// Information about each course
+let HTML1 = ["example.png", "Codecademy HTML Course", "This course teaches you how to make HTML from scratch!", "https://www.codecademy.com/learn/learn-html"]
+let CSS1 = ["example.png", "Codecademy CSS Course", "This course teaches you how to use CSS styling.", "https://www.codecademy.com/learn/learn-css"]
+let HTML2 = ["example.png", "FreeCodeCamp HTML Course", "This course teaches you about HTML.", "https://www.freecodecamp.org/news/learn-html-beginners-course/"]
+let CSS2 = ["example.png", "FreeCodeCamp CSS Course", "This course teaches you how to add CSS to your website.", "https://www.freecodecamp.org/news/learn-css-in-11-hours/"]
+// Here, the createCard function is used to make a card for each of the course variables above.
+createCard(HTML1)
+createCard(CSS1)
+createCard(HTML2)
+createCard(CSS2)
+
+
+
