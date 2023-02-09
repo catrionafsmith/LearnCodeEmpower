df78ad7a4f86cfecfa6377c33c91e94340dfac0a
Katy Smith <113362369+catrionafsmith@users.noreply.github.com>
2023-02-09 18:29:13 +1100

    Fix chatbot terminal and size of icons

---
+++ chatscript.js
@@ -0,0 +1,37 @@
+
+$("#terminal").terminal(async function (command, terminal) {
+    try {
+        const prompt = `you are a helpful, knowledge sharing careers chatbot. I say: ${command}. You reply:`
+        const keyresp = await fetch('/.netlify/functions/hello')
+        .then(response => response.json()
+        )
+        //  console.log(keyresp['message']);
+
+        const response = await fetch(
+            `https://api.openai.com/v1/completions`,
+            {
+                body: JSON.stringify({"model": "text-davinci-003", "prompt": prompt, "temperature": 0.86, "max_tokens": 256}),
+                method: "POST",
+                headers: {
+                    "content-type": "application/json",
+                    Authorization:`Bearer ` + keyresp['message'],
+                },
+                    }
+        ).then((response) => {
+            if (response.ok) {
+                response.json().then((json) => {
+                    terminal.echo(json.choices[0].text.trim(), {keepWords: true});
+                });
+            }
+        });
+      
+        console.log("Completed!");
+    } catch (err) { console.error(`Error: ${err}`) }
+},
+    {
+        greetings: 'Careers Chatbot v1: Type your question at the arrow, then press enter. Wait 5-10 seconds for your personalised reply!',
+        name: 'gpt3_demo',
+        height: 400,
+        // width: 800,
+        prompt: '> ',
+    });
