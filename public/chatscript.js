79ee90dea5293e8ffdca35541a194a76c9fa4486
Katy Smith <113362369+catrionafsmith@users.noreply.github.com>
2023-02-09 19:45:37 +1100

    Edit chatbot terminal

--- public/chatscript.js
+++ public/chatscript.js
@@ -29,7 +29,7 @@ $("#terminal").terminal(async function (command, terminal) {
     } catch (err) { console.error(`Error: ${err}`) }
 },
     {
-        greetings: 'Careers Chatbot v1: Type your question at the arrow, then press enter. Wait 5-10 seconds for your personalised reply!',
+        greetings: 'Careers Chatbot v1:',
         name: 'gpt3_demo',
         height: 400,
         // width: 800,
